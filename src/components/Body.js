a385e1ba9086f648a5b173db870185ccc6b9a33b
Oleh Ozhinskyi <ozolezhka@gmail.com>
2025-12-01 20:12:47 +0200

    Add info about Oleh Ozhinskyi

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Michael_kaiser.jpg"} alt="Oleh Ozhinskyi" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Oleh Ozhinskyi</h2>
+          <p style={styles.position}>Project Manager</p>
         </div>
       </div>
     </main>
