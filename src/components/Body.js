9465abc515d9401550fc6d8c8525011e2ddc5049
p4mp3rs <128310751+p4mp3rs@users.noreply.github.com>
2025-12-01 20:19:53 +0200

    Add info about Nazarii

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar.png"} alt="Nazarii Kukiz" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Nazarii Kukiz</h2>
+          <p style={styles.position}>Student</p>
         </div>
       </div>
     </main>
