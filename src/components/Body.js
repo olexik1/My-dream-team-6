f55708e5ad192cfa7c5d0b926eec9340868f90af
Mariteyaa <kmariana4rf@gmail.com>
2025-12-01 20:21:10 +0200

    Add info about Mariteyaa

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/NanaPic.jpg"} alt="Mariana Kushnir" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
+          <h2 style={styles.name}>Mariana Kushnir</h2>
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
