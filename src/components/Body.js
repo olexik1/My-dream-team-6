8b4713bdca5461d4af85887a179114f23a58968a
BETAJLb07 <nahirnyvitalik81@gmail.com>
2025-12-02 10:15:58 +0200

    Our Team Lead

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,34 +3,6 @@
 const Body = () => {
   return (
     <main style={styles.body}>
-      <div style={styles.card}>
-        <img src={"/NanaPic.jpg"} alt="Mariana Kushnir" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Mariana Kushnir</h2>
-          <p style={styles.position}>Software engineer</p>
-       </div>
-       </div>
-       <div style={styles.card}>
-        <img src={"/avatar.png"} alt="Nazarii Kukiz" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Nazarii Kukiz</h2>
-          <p style={styles.position}>Student</p>
-        </div>
-        </div>
-        <div style={styles.card}>
-        <img src={"/Michael_kaiser.jpg"} alt="Oleh Ozhinskyi" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Oleh Ozhinskyi</h2>
-          <p style={styles.position}>Project Manager</p>
-        </div>
-        </div>
-        <div style={styles.card}>
-        <img src={"/SitePhoto.png"} alt="Nahirnyi Vitalii" style={styles.photo} />
-        <div style={styles.textContainer}>
-          <h2 style={styles.name}>Nahirnyi Vitalii</h2>
-          <p style={styles.position}>Student at Lviv Polytechnic</p>
-        </div>
-          </div>
           <div style={styles.card}>
               <img src={"/Murzik.png"} alt="Murzik" style={styles.photo} />
               <div style={styles.textContainer}>
