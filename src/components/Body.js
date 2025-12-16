2ca5cd6704b90216b7749aef135c6c3ed21492f9
BETAJLb07 <nahirnyvitalik81@gmail.com>
2025-12-16 10:51:09 +0200

    test2

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,7 +8,7 @@ const Body = () => {
               <img src={"/Murzik.png"} alt="Barsik" style={styles.photo} />
               <div style={styles.textContainer}>
                   <h2 style={styles.name}>Barsik</h2>
-                  <p style={styles.position}>Team Lead(test)</p>
+                  <p style={styles.position}>Team Lead(test234)</p>
               </div>
           </div>
       <div style={styles.card}>
